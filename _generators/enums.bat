42e8f3a82be3bab47b9a393ad1f13d0ad1b27b30
al-martyn1 <amart@mail.ru>
2023-05-12 03:28:51 +0300

    Начал переходить на тул

--- _generators/enums.bat
+++ _generators/enums.bat
@@ -1,8 +1,10 @@
-rem 
-rem 
-call cxx17 enums.cpp
-enums.exe > ..\enums.h
+@rem rem 
+@rem rem 
+@rem call cxx17 enums.cpp
+@rem enums.exe > ..\enums.h
+@rem 
+@rem if exist enums.exe del enums.exe
+@rem if exist enums.obj del enums.obj
+@rem 
 
-if exist enums.exe del enums.exe
-if exist enums.obj del enums.obj
 
